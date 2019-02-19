009d3012fea6e34b78388ac3173a2832913deff5
Arkady Zelensky <arkasha9606@gmail.com>
2019-02-19 16:59:45 +0200

    Updates

--- precache-manifest.9872b6c62bdfaf65533af88f42176c0e.js
+++ precache-manifest.9872b6c62bdfaf65533af88f42176c0e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/tooltip-image/static/js/runtime~main.1b453d2f.js"
   },
   {
-    "revision": "923882344d18259bfe11",
-    "url": "/tooltip-image/static/js/main.92388234.chunk.js"
+    "revision": "7b4eaec55cf8cd919d06",
+    "url": "/tooltip-image/static/js/main.7b4eaec5.chunk.js"
   },
   {
     "revision": "9f8409b9c3b052a21493",
     "url": "/tooltip-image/static/js/2.9f8409b9.chunk.js"
   },
   {
-    "revision": "923882344d18259bfe11",
+    "revision": "7b4eaec55cf8cd919d06",
     "url": "/tooltip-image/static/css/main.785f5d9f.chunk.css"
   },
   {
-    "revision": "c88edf7d426c6e85b5dc1479701fd772",
+    "revision": "9d662298b78674b80d05539cec3ff799",
     "url": "/tooltip-image/index.html"
   }
 ];
