ca1400ba72228d54fe4a596d90d3c59f37d29056
Arkady Zelensky <arkasha9606@gmail.com>
2019-02-19 15:39:23 +0200

    add deploy functionality

---
+++ build/precache-manifest.cdd9b657f1d30651e2919c40de4e16ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b453d2ff71b59e19c51",
+    "url": "/tooltip-image/static/js/runtime~main.1b453d2f.js"
+  },
+  {
+    "revision": "9b0a8414b9bdec323d43",
+    "url": "/tooltip-image/static/js/main.9b0a8414.chunk.js"
+  },
+  {
+    "revision": "9f8409b9c3b052a21493",
+    "url": "/tooltip-image/static/js/2.9f8409b9.chunk.js"
+  },
+  {
+    "revision": "9b0a8414b9bdec323d43",
+    "url": "/tooltip-image/static/css/main.9d107565.chunk.css"
+  },
+  {
+    "revision": "a2dccbd66bc11194fefb194e4800162e",
+    "url": "/tooltip-image/index.html"
+  }
+];
